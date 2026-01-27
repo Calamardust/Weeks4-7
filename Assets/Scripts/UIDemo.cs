4cbda0c01805c6145f176a716d69373f25ae3966
Daniel Alejandro Diaz Peraza <diazperd@shernet.sheridancollege.ca>
2026-01-27 10:53:19 -0500

    First 2 scripts and UI

---
+++ Assets/Scripts/UIDemo.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIDemo : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            ChangeColor();
+        }
+    }
+
+   public void ChangeColor()
+    {
+        sr.color = Random.ColorHSV();
+    }
+
+    public void SetScale(float scale)
+    {
+        transform.localScale = Vector3.one * scale;
+    }
+}
